172664002daff5d7c6441162038867183bfc16d3
DOOM\user <igruppa4@mail.ru>
2023-11-04 00:22:59 +0300

    edit s4, finished

---
+++ s4/src/components/HeaderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header center">
+    <a href="#" class="header__link-logo">
+      <img
+        :src="require('@/assets/img/logo.svg')"
+        alt="logo"
+        class="header__logo"
+      />
+    </a>
+    <nav class="header__nav">
+      <a href="#" class="header__nav-link">Home</a>
+      <a href="#" class="header__nav-link">Project</a>
+      <a href="#" class="header__nav-link">Blog</a>
+    </nav>
+  </header>
+</template>
+
+
+<style lang="scss" scoped></style>
